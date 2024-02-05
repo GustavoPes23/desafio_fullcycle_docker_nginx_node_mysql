741a26a7c61ba042ba6596cbc6946b20e403b3fe
Gustavo Pes <95422699+GustavoPes23@users.noreply.github.com>
2024-02-05 19:02:18 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const bindAddress = "0.0.0.0";
+const config = {
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "nodedb",
+};
+const mysql = require("mysql");
+const connection = mysql.createConnection(config);
+
+const sqlCreate = `CREATE TABLE IF NOT EXISTS people (
+    id int(11) NOT NULL AUTO_INCREMENT,
+    name varchar(255) DEFAULT NULL,
+    PRIMARY KEY (id)
+) `;
+connection.query(sqlCreate);
+
+app.get("/", (_, res) => {
+    const sqlInsert = `INSERT INTO people(name) values('Gustavo')`;
+    connection.query(sqlInsert);
+
+    let html = "<h1>Full Cycle </h1><ul>";
+
+    const sqlSelect = "SELECT * FROM people";
+    connection.query(sqlSelect, function (err, result) {
+        if (err) {
+            throw err;
+        }
+
+        result.forEach((item) => {
+            html += `<li>${item.name}</li>`;
+        });
+
+        html += `</ul>`;
+        res.send(html);
+    });
+});
+
+app.listen(port, bindAddress, (err) => {
+  if (err) {
+    console.error(`Error starting server: ${err}`);
+  } else {
+    console.log("Rodando na porta " + port);
+  }
+});
